a10cbf89f161824ec4f3d33872c562b44f7a6c06
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-19 22:51:53 +0300

    Add SharesController with CRUD methods

---
+++ src/Controllers/SharesController.cs
@@ -0,0 +1,39 @@
+using EvaExchange.API.Application.Shares.Commands;
+using EvaExchange.API.Application.Shares.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvaExchange.API.Controllers;
+
+[Route("api/v1/[controller]")]
+public class SharesController(ISender mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetShares()
+    {
+        var shares = await mediator.Send(new GetSharesQuery());
+        return Ok(shares);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetShare(string id)
+    {
+        var share = await mediator.Send(new GetShareQuery(id));
+        return share is null ? NotFound() : Ok(share);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateShare([FromBody] CreateShareCommand command)
+    {
+        var share = await mediator.Send(command);
+        return CreatedAtAction(nameof(GetShare), new {id = share.Id}, share);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateShare([FromRoute]string id, [FromBody] UpdateShareCommand command)
+    {
+        command.Id = id;
+        var share = await mediator.Send(command);
+        return share is null ? NotFound() : Ok(share);
+    }
+}
