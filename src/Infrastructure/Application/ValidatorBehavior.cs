2973a7526baa95a9e15547245e2065fcece97517
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-19 22:29:35 +0300

    Add FluentValidation package and implement ValidatorBehavior for MediatR

---
+++ src/Infrastructure/Application/ValidatorBehavior.cs
@@ -0,0 +1,35 @@
+using EvaExchange.API.Extensions;
+using FluentValidation;
+using MediatR;
+
+namespace EvaExchange.API.Infrastructure.Application;
+
+public class ValidatorBehavior<TRequest, TResponse>(
+    IEnumerable<IValidator<TRequest>> validators,
+    ILogger<ValidatorBehavior<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        var typeName = request.GetGenericTypeName();
+
+        logger.LogInformation("Validating command {CommandType}", typeName);
+
+        var failures = validators
+            .Select(v => v.Validate(request))
+            .SelectMany(result => result.Errors)
+            .Where(error => error != null)
+            .ToList();
+
+        if (failures.Count == 0)
+            return await next();
+
+        logger.LogWarning("Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}",
+            typeName,
+            request,
+            failures);
+
+        throw new ValidationException($"Command Validation Errors for type {typeName}", failures);
+    }
+}
