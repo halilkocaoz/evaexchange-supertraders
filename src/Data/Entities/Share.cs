888aaa06ab074e8cd78e2993677c1c1814eaaf8e
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-19 22:05:27 +0300

    update Share entity to add constructor and add updating logics method

--- src/Data/Entities/Share.cs
+++ src/Data/Entities/Share.cs
@@ -2,8 +2,23 @@ namespace EvaExchange.API.Data.Entities;
 
 public class Share : BaseEntity
 {
-    public string Id { get; set; }
+    public Share(string id, decimal rate, decimal price)
+    {
+        Id = id;
+        Rate = rate;
+        Price = price;
+    }
     
-    public decimal Rate { get; set; }
-    public decimal Price { get; set; }
+    public void Update(decimal price)
+    {
+        if (UpdatedAt.HasValue && UpdatedAt.Value > DateTime.UtcNow.AddHours(-1))
+            throw new Exception("Cannot update more than once per one hour.");
+        
+        Price = price;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    
+    public string Id { get; init; }
+    public decimal Rate { get; init; }
+    public decimal Price { get; private set; }
 }
