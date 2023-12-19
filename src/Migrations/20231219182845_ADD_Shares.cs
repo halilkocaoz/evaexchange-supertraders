970c24479d5755b6ae8107fbb41af6a376fe03f4
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-19 21:31:26 +0300

    init database migration with shares table

---
+++ src/Migrations/20231219182845_ADD_Shares.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvaExchange.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADD_Shares : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shares",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "char(3)", maxLength: 3, nullable: false),
+                    Rate = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shares", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shares");
+        }
+    }
+}
