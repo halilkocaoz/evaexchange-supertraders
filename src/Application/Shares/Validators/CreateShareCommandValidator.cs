d4c907744f791c82babf3e9195b1f4dd0a6200e5
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-20 21:09:32 +0300

    update validator regex

--- src/Application/Shares/Validators/CreateShareCommandValidator.cs
+++ src/Application/Shares/Validators/CreateShareCommandValidator.cs
@@ -7,8 +7,8 @@ public class CreateShareCommandValidator : AbstractValidator<CreateShareCommand>
 {
     public CreateShareCommandValidator()
     {
-        RuleFor(x => x.Id).NotEmpty().MaximumLength(3).Matches("^[A-Z]")
-            .WithMessage($"The '{nameof(CreateShareCommand.Id)}' contain only capital letters.");
+        RuleFor(x => x.Id).NotEmpty().MaximumLength(3).Matches("^[A-Z]+$")
+            .WithMessage($"The '{nameof(CreateShareCommand.Id)}' must be only capital letters.");
         RuleFor(x => x.Rate).NotNull().GreaterThan(0).LessThan(100);
         RuleFor(x => x.Price).NotNull().GreaterThan(0);
     }
